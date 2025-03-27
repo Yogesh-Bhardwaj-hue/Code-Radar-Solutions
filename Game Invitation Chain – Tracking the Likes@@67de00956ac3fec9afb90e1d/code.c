d2a8bd9107f4ff5f2510853a0064592f56af58ea
Yogesh  . <astasaras19@gmail.com>
2025-03-27 18:38:35 +0530

    Solution to Game Invitation Chain – Tracking the Likes

--- Game Invitation Chain – Tracking the Likes@@67de00956ac3fec9afb90e1d/code.c
+++ Game Invitation Chain – Tracking the Likes@@67de00956ac3fec9afb90e1d/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 int totalLikes(int n){
-    int initial =5;
+    int initial =5,likes;
     for(int i=1; i<=n; i++){
-        int likes = initial/2;
+        likes = initial/2;
         initial+=3;
 
     }
