10a367fd3b553ad0518bd62e340d80d2b7f5afaf
Yogesh  . <astasaras19@gmail.com>
2025-03-28 18:23:21 +0530

    Solution to Gadget Combo Hunt – Spend Smart

--- Gadget Combo Hunt – Spend Smart@@67de00956ac3fec9afb90e1c/code.c
+++ Gadget Combo Hunt – Spend Smart@@67de00956ac3fec9afb90e1c/code.c
@@ -1,10 +1,8 @@
 int maxGadgetSpend(int keyboards[],int n, int headsets[], int m , int budget){
     for(int i=1;i<=budget;i++){
-        if(keyboards[i]==headsets[i]){
+        while(keyboards[i]==headsets[i]){
             i+=i;
             return i;        }
     }
-    else{
-        return -1;
-    }
+   
 }
