dfd23c07658982bacc71476c7e9f7fd9bb805d7a
Yogesh  . <astasaras19@gmail.com>
2025-04-04 16:41:38 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -6,14 +6,4 @@ void Fibo(int *a, int *b, int *c) {
     *b = *c;       
 }
 
-int main() {
-    int a = 0, b = 1, c, n;
-    scanf("%d %d", &n);
-    for (int i = 2; i < n; i++) { 
-        Fibo(&a, &b, &c);
-        printf(" %d", c);
-    }
-    printf("\n");
 
-    return 0;
-}
