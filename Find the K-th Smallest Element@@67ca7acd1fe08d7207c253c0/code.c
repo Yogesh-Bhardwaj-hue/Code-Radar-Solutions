da86ececd2ceedac1be9c1952b08ffc741ab4514
Yogesh  . <astasaras19@gmail.com>
2025-04-07 20:22:07 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -2,7 +2,11 @@ int kthSmallest(int arr[] , int n , int k){
     int temp;
      for(int i = 0; i < n - 1; i++) {
         for(int j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
+            if(k > n){
+                return -1;
+                break;
+            }
+            else if(arr[j] > arr[j + 1]) {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
