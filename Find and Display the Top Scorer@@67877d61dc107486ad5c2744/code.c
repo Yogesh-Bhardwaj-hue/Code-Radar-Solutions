24bbf86530312668405a2408ed1c7b4cce81b6da
Yogesh  . <astasaras19@gmail.com>
2025-04-08 18:37:34 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -1,27 +1,29 @@
-#include <stdio.h>  
+#include <stdio.h>
 
-struct Student {  
-    int rollNumber;  
-    char name[50];  
-    float marks;  
-};  
+struct Student {
+    int rollNumber;
+    char name[50];
+    float marks;
+};
 
-int main() {  
-    int N;   
-    scanf("%d", &N);  
-    struct Student students[N];   
-    for (int i = 0; i < N; i++) {   
-        scanf("%d %s %f", &students[i].rollNumber, students[i].name, &students[i].marks);  
-    }  
+int main() {
+    int N;
+    scanf("%d", &N);
+    struct Student students[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d %s %f", &students[i].rollNumber, students[i].name, &students[i].marks);
+    }
 
-    int maxmarks = &students.marks[0];
-    for (int i = 0; i < N; i++) {  
-        if(&student.marks[i]>maxmarks){
-            maxmarks = &students.marks[i] ;
-        printf("Roll Number: %d, Name: %s, Marks: %.2f\n",   
-               students[i].rollNumber, students[i].name, students[i].marks);  
+    int topIndex = 0;
+    for (int i = 1; i < N; i++) {
+        if (students[i].marks > students[topIndex].marks) {
+            topIndex = i;
         }
-    }  
+    }
+    printf("Roll Number: %d, Name: %s, Marks: %.2f\n",
+           students[topIndex].rollNumber,
+           students[topIndex].name,
+           students[topIndex].marks);
 
-    return 0;  
+    return 0;
 }
