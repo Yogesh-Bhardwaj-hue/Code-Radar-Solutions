04ad85a4f870d30bb2b24d32265914c66e82ca7c
Yogesh  . <astasaras19@gmail.com>
2025-04-08 12:08:53 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int main() {
     int n;
     scanf("%d", &n);
-    int arr[n];
 
+    if(n < 2) {
+        printf("-1\n");  
+        return 0;
+    }
+
+    int arr[n];
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    for(int i=0 ; i<n ; i++){
-        for(int j=0 ; j<n -1- i ; j++){
-            if(arr[j]>arr[j+1]){
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1]=temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    printf("%d %d",arr[n-2],arr[n-1]);
+
+    int minDiff = INT_MAX;
+    int num1 = 0, num2 = 0;
+
+    for(int i = 0; i < n - 1; i++) {
+        int diff = arr[i + 1] - arr[i];
+        if(diff < minDiff) {
+            minDiff = diff;
+            num1 = arr[i];
+            num2 = arr[i + 1];
+        }
+        else if(diff == minDiff && arr[i] < num1) {
+            num1 = arr[i];
+            num2 = arr[i + 1];
+        }
+    }
+
+    printf("%d %d\n", num1, num2);
     return 0;
 }
